3511de078463e42dfb427fd0edaac13715a620dc
Jeisson <jeissonstewen@gmail.com>
2022-12-05 10:03:14 -0500

    arreglos

--- main.js
+++ main.js
@@ -26,5 +26,7 @@ guardar.addEventListener("click",()=>{
     <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">camisas</td>
     
 
-</tr> `
+</tr> `;
+modal.classList.remove("scale-100");
+  
 })
